cf3bc311cd418ff51d3a68e9d7122a296ac2e8dc
Rafael Santos <rafaelcs_94@hotmail.com>
2022-09-20 20:40:29 -0300

    test: :white_check_mark: adds test for report of Product class

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    sut_product = Product(
+        "randomID",
+        "Produto Genérico",
+        "Empresa Genérica",
+        2022,
+        2025,
+        "123456789",
+        "em temperatura ambiente",
+    )
+
+    assert sut_product.__repr__() == (
+        "O produto Produto Genérico"
+        " fabricado em 2022"
+        " por Empresa Genérica com validade"
+        " até 2025"
+        " precisa ser armazenado em temperatura ambiente."
+    )
