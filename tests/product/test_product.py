bca19c8afe317e3925289e360cf1c010677f74d1
Rafael Santos <rafaelcs_94@hotmail.com>
2022-09-20 20:39:44 -0300

    test: :white_check_mark: adds test for Product class

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        "randomID",
+        "Produto Genérico",
+        "Empresa Genérica",
+        2022,
+        2025,
+        "123456789",
+        "temperatura ambiente",
+    )
+
+    assert new_product.id == "randomID"
+    assert new_product.nome_do_produto == "Produto Genérico"
+    assert new_product.nome_da_empresa == "Empresa Genérica"
+    assert new_product.data_de_fabricacao == "2022"
+    assert new_product.data_de_validade == "2025"
+    assert new_product.numero_de_serie == "123456789"
+    assert new_product.instrucoes_de_armazenamento == "temperatura ambiente"
