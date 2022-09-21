63bc2ef10dac38d7142d0e25747d914082d27a30
Rafael Santos <rafaelcs_94@hotmail.com>
2022-09-20 21:49:28 -0300

    feat: :sparkles: Implements import_data method to read json and xml files

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -9,13 +11,42 @@ class Inventory:
 
     @staticmethod
     def import_data(path, type):
-        with open(path) as file:
-            reader = csv.DictReader(file)
-            data = []
-            for row in reader:
-                data.append(row)
+        if path.endswith('.csv'):
+            with open(path) as file:
+                reader = csv.DictReader(file)
+                data = []
+                for row in reader:
+                    data.append(row)
 
-            if type == "simples":
-                return SimpleReport.generate(data)
-            else:
-                return CompleteReport.generate(data)
+                if type == "simples":
+                    return SimpleReport.generate(data)
+                else:
+                    return CompleteReport.generate(data)
+
+        elif path.endswith('.json'):
+            with open(path) as file:
+                data = json.loads(file.read())
+
+                if type == "simples":
+                    return SimpleReport.generate(data)
+                else:
+                    return CompleteReport.generate(data)
+
+        else:
+            with open(path) as file:
+                tree = ET.parse(file)
+                root = tree.getroot()
+                data = []
+
+                product = {}
+                for child in root.iter():
+                    if '\n' not in child.text:
+                        product[child.tag] = child.text
+                    if child.tag == 'instrucoes_de_armazenamento':
+                        data.append(product)
+                        product = {}
+
+                if type == "simples":
+                    return SimpleReport.generate(data)
+                else:
+                    return CompleteReport.generate(data)
